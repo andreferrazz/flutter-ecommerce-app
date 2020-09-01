3738f35fc8b2957b9b9557d7f8090532c7dbdab3
Andre Ferraz <andreferraz.dv@gmail.com>
2020-09-01 00:18:35 -0300

    Tabbed navigation on the home screen

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,13 +1,30 @@
 import 'package:e_commerce/services/auth.dart';
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   final AuthService _auth = AuthService();
+
+  int _currentIndex = 0;
+
+  final _tabs = [
+    Center(child: Text('Home')),
+    Center(child: Text('Cart')),
+    Center(child: Text('Favorite')),
+    Center(child: Text('Profile')),
+  ];
+
+  final _titles = ['Home', 'Cart', 'Favorite', 'Profile'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text(_titles[_currentIndex]),
         actions: <Widget>[
           FlatButton.icon(
             onPressed: () async {
@@ -19,7 +36,35 @@ class Home extends StatelessWidget {
           )
         ],
       ),
-      body: Center(child: Text('Home')),
+      body: _tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            title: Text('Home'),
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            title: Text('Cart'),
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            title: Text('Favorite'),
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            title: Text('Profile'),
+            backgroundColor: Colors.blue,
+          ),
+        ],
+        onTap: (index) {
+          setState(() => _currentIndex = index);
+        },
+      ),
     );
   }
 }
