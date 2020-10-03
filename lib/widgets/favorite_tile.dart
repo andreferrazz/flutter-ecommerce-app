b39da7d49463471afdc54969d6bcea4b3296617f
Andre Ferraz <andreferraz.dv@gmail.com>
2020-10-02 23:06:42 -0300

    UI feature: added navigation to this ProductDetails from FavoriteTile

--- lib/widgets/favorite_tile.dart
+++ lib/widgets/favorite_tile.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:e_commerce/screens/details/product_details.dart';
 import 'package:e_commerce/shared/constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -45,75 +46,84 @@ class _FavoriteTileState extends State<FavoriteTile> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Row(
-        children: <Widget>[
-          Flexible(
-            flex: 1,
-            child: Image.network(
-              _imgUrl,
-              fit: BoxFit.cover,
-              height: _height,
+      child: GestureDetector(
+        onTap: (){
+          // TODO: navigate to product detail screen
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => ProductDetails()),
+          );
+        },
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              flex: 1,
+              child: Image.network(
+                _imgUrl,
+                fit: BoxFit.cover,
+                height: _height,
+              ),
             ),
-          ),
-          Flexible(
-            flex: 2,
-            child: Container(
-              width: 1 / 0,
-              height: _height,
-              child: Stack(
-                children: <Widget>[
-                  Positioned(
-                    top: 0,
-                    left: 0,
-                    child: Container(
-                      padding: EdgeInsets.all(16.0),
-                      height: _height,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text(
-                            'Title',
-                            style: TextStyle(
-                              color: Colors.grey[800],
-                              fontSize: 18.0,
+            Flexible(
+              flex: 2,
+              child: Container(
+                width: 1 / 0,
+                height: _height,
+                child: Stack(
+                  children: <Widget>[
+                    Positioned(
+                      top: 0,
+                      left: 0,
+                      child: Container(
+                        padding: EdgeInsets.all(16.0),
+                        height: _height,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              'Title',
+                              style: TextStyle(
+                                color: Colors.grey[800],
+                                fontSize: 18.0,
+                              ),
                             ),
+                            Text(
+                              '\$ 100.00',
+                              style: TextStyle(
+                                color: Colors.grey[800],
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 0.0,
+                      right: 0,
+                      child: PopupMenuButton<FavoriteOption>(
+                        icon: Icon(Icons.more_vert, color: Colors.grey[800]),
+                        onSelected: _onSelectedPopupMenuOption,
+                        itemBuilder: (_) => <PopupMenuItem<FavoriteOption>>[
+                          PopupMenuItem<FavoriteOption>(
+                            value: FavoriteOption.REMOVE,
+                            child: Text('Remove'),
                           ),
-                          Text(
-                            '\$ 100.00',
-                            style: TextStyle(
-                              color: Colors.grey[800],
-                              fontSize: 24.0,
-                              fontWeight: FontWeight.bold,
-                            ),
+                          PopupMenuItem<FavoriteOption>(
+                            value: FavoriteOption.ADD_CART,
+                            child: Text('Add to cart'),
                           ),
                         ],
                       ),
                     ),
-                  ),
-                  Positioned(
-                    top: 0.0,
-                    right: 0,
-                    child: PopupMenuButton<FavoriteOption>(
-                      icon: Icon(Icons.more_vert, color: Colors.grey[800]),
-                      onSelected: _onSelectedPopupMenuOption,
-                      itemBuilder: (_) => <PopupMenuItem<FavoriteOption>>[
-                        PopupMenuItem<FavoriteOption>(
-                          value: FavoriteOption.REMOVE,
-                          child: Text('Remove'),
-                        ),
-                        PopupMenuItem<FavoriteOption>(
-                          value: FavoriteOption.ADD_CART,
-                          child: Text('Add to cart'),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
